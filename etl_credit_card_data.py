bfa525db5a47d19a556e125e18c936fb6ff0aca3
Tek <46939335+teksongeap@users.noreply.github.com>
2024-06-19 11:56:21 -0500

    etl complete

--- etl_credit_card_data.py
+++ etl_credit_card_data.py
@@ -1,11 +1,12 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import initcap, concat, lit, lower, substring, unix_timestamp, lpad, col, coalesce
+from pyspark.sql.functions import initcap, concat, lit, lower, substring, lpad
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, TimestampType
+from mysql_db_setup import create_db_and_tables_with_keys
 
 # Create spark session
 spark = SparkSession.builder.appName('ETL_credit_card_data').getOrCreate()
 
-# ----EXTRACT----
+# -----EXTRACT-----
 
 # Create schema
 customer_schema = StructType([
@@ -26,6 +27,19 @@ customer_schema = StructType([
     StructField("_corrupt_record", StringType(), True)  # To handle corrupt records
 ])
 
+branch_schema = StructType([
+    StructField("BRANCH_CODE", IntegerType(), True),
+    StructField("BRANCH_NAME", StringType(), True),
+    StructField("BRANCH_STREET", StringType(), True),
+    StructField("BRANCH_CITY", StringType(), True),
+    StructField("BRANCH_STATE", StringType(), True),
+    StructField("BRANCH_ZIP", StringType(), True),
+    StructField("BRANCH_PHONE", StringType(), True),
+    StructField("LAST_UPDATED", TimestampType(), True),
+    StructField("_corrupt_record", StringType(), True)  
+])
+
+
 credit_schema = StructType([
     StructField("TRANSACTION_ID", IntegerType(), True),
     StructField("DAY", StringType(), True),
@@ -39,29 +53,19 @@ credit_schema = StructType([
     StructField("_corrupt_record", StringType(), True)  
 ])
 
-branch_schema = StructType([
-    StructField("BRANCH_CODE", IntegerType(), True),
-    StructField("BRANCH_NAME", StringType(), True),
-    StructField("BRANCH_STREET", StringType(), True),
-    StructField("BRANCH_CITY", StringType(), True),
-    StructField("BRANCH_STATE", StringType(), True),
-    StructField("BRANCH_ZIP", StringType(), True),
-    StructField("BRANCH_PHONE", StringType(), True),
-    StructField("LAST_UPDATED", TimestampType(), True),
-    StructField("_corrupt_record", StringType(), True)  
-])
 
 # Read json into dataframe using respective schema
 # Multiline option required for json objects that take up multiple lines
-customer_df = spark.read.option("multiline", "true").schema(customer_schema).json('cdw_sapp_customer.json')
-credit_df = spark.read.option("multiline", "true").schema(credit_schema).json('cdw_sapp_credit.json')
-branch_df = spark.read.option("multiline", "true").schema(branch_schema).json('cdw_sapp_branch.json')
+customer_df = spark.read.option("multiline", "true").schema(customer_schema).json('json/cdw_sapp_customer.json')
+branch_df = spark.read.option("multiline", "true").schema(branch_schema).json('json/cdw_sapp_branch.json')
+credit_df = spark.read.option("multiline", "true").schema(credit_schema).json('json/cdw_sapp_credit.json')
 
-# ----TRANSFORM----
+# -----TRANSFORM-----
+
 # Drop corrupted records, if any
 customer_df = customer_df.drop("_corrupt_record")
-credit_df = credit_df.drop("_corrupt_record")
 branch_df = branch_df.drop("_corrupt_record")
+credit_df = credit_df.drop("_corrupt_record")
 
 # Map the data to required format for database
 # Pyspark substring() function is 1-based index, not 0-based, for some reason
@@ -71,12 +75,12 @@ customer_df = customer_df.withColumn("FIRST_NAME", initcap(customer_df["FIRST_NA
                          .withColumn("FULL_STREET_ADDRESS", concat(customer_df["STREET_NAME"], lit(", "), customer_df["APT_NO"])) \
                          .withColumn("CUST_PHONE", concat(substring(customer_df["CUST_PHONE"], 1, 3), lit("-"), substring(customer_df["CUST_PHONE"], 4, 4)))
 
-# Add leading zeros to MONTH and DAY column using lpad() so that it is regular
-credit_df = credit_df.withColumn("DAY", lpad(credit_df["DAY"], 2, '0')) \
-                     .withColumn("MONTH", lpad(credit_df["MONTH"], 2, '0'))
-    
-# Convert YEAR, MONTH, and DAY column into TIME_ID using concat()
-credit_df = credit_df.withColumn("TIME_ID", concat(credit_df["YEAR"], credit_df["MONTH"], credit_df["DAY"]))
+# Drop the extraneous columns
+customer_df = customer_df.drop("STREET_NAME", "APT_NO")
+
+# Reorder columns to match specs
+customer_df = customer_df.select("SSN", "FIRST_NAME", "MIDDLE_NAME", "LAST_NAME", "CREDIT_CARD_NO", "FULL_STREET_ADDRESS",
+                                 "CUST_CITY", "CUST_STATE", "CUST_COUNTRY", "CUST_ZIP", "CUST_PHONE", "CUST_EMAIL", "LAST_UPDATED")
 
 # Use fillna() directly on branch_df, passing a dictionary
 branch_df = branch_df.fillna({"BRANCH_ZIP": "99999"})
@@ -84,8 +88,57 @@ branch_df = branch_df.fillna({"BRANCH_ZIP": "99999"})
 # Convert BRANCH_PHONE in branch_df to (###)###-#### format
 branch_df = branch_df.withColumn("BRANCH_PHONE", concat(lit('('), substring(branch_df["BRANCH_PHONE"], 1, 3), lit(')'), 
                                     substring(branch_df["BRANCH_PHONE"], 4, 3), lit('-'), substring(branch_df["BRANCH_PHONE"], 7, 4)))
-print(customer_df.show(truncate=False))
-print(credit_df.show(truncate=False))
-print(branch_df.show(truncate=False))
 
+# Reorder columns
+branch_df = branch_df.select("BRANCH_CODE", "BRANCH_NAME", "BRANCH_STREET", "BRANCH_CITY", "BRANCH_STATE", "BRANCH_ZIP",
+                             "BRANCH_PHONE", "LAST_UPDATED")
+
+# Add leading zeros to MONTH and DAY column using lpad() so that it is regular
+credit_df = credit_df.withColumn("DAY", lpad(credit_df["DAY"], 2, '0')) \
+                     .withColumn("MONTH", lpad(credit_df["MONTH"], 2, '0'))
+    
+# Convert YEAR, MONTH, and DAY column into TIME_ID using concat()
+credit_df = credit_df.withColumn("TIME_ID", concat(credit_df["YEAR"], credit_df["MONTH"], credit_df["DAY"]))
+
+# Drop the extraneous columns
+credit_df = credit_df.drop("DAY", "MONTH", "YEAR")
+
+# Rename column
+credit_df = credit_df.withColumnRenamed("CREDIT_CARD_NO", "CUST_CC_NO")
+
+# Reorder columns
+credit_df = credit_df.select("TRANSACTION_ID", "TIME_ID", "CUST_CC_NO", "CUST_SSN", "BRANCH_CODE", "TRANSACTION_TYPE", "TRANSACTION_VALUE")
+
+print(customer_df.show(truncate=False))
+print(branch_df.show(truncate=False))
+print(credit_df.show(truncate=False))
+
+# -----LOAD-----
+
+# Create the database and the tables
+create_db_and_tables_with_keys("localhost", "root", "password", "creditcard_capstone")
+
+# Arguments for writing to the database using pyspark
+jdbc_url = "jdbc:mysql://localhost:3306/creditcard_capstone"
+connection_prop = {
+    "user": "root",
+    "password": "password",
+    "driver": "com.mysql.cj.jdbc.Driver"
+}
+
+# Function for writing to database using the "append" mode
+# Using "overwrite" mode does not work since the tables already have foreign keys, so they can't be dropped
+def write_dataframe_to_mysql(df, table_name):
+    try:
+        df.write.jdbc(url=jdbc_url, table=table_name, mode="append", properties=connection_prop)
+        print(f"Successfully wrote DataFrame to table {table_name}")
+    except Exception as e:
+        print(f"Error writing DataFrame to table {table_name}: {e}")
+
+# Write DataFrames to MySQL tables with error handling
+write_dataframe_to_mysql(customer_df, "CDW_SAPP_CUSTOMER")
+write_dataframe_to_mysql(branch_df, "CDW_SAPP_BRANCH")
+write_dataframe_to_mysql(credit_df, "CDW_SAPP_CREDITCARD")
+
+# Stop the session
 spark.stop()
---
+++ mysql_db_setup.py
@@ -0,0 +1,104 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+# Creating database and creating tables must be separate
+def create_database(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            password=user_password
+        )
+        if connection.is_connected():
+            cursor = connection.cursor()
+            # SQL command for creating the database
+            cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
+            print(f"Database '{db_name}' created successfully")
+    except Error as e:
+        print(f"Error: '{e}'")
+    finally:
+        if connection and connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection for database creation is closed")
+
+
+# Create the database and tables CDW_SAPP_CUSTOMER, CDW_SAPP_CREDITCARD, CDW_SAPP_BRANCH if they don't exist            
+def create_db_and_tables_with_keys(host_name, user_name, user_password, db_name):
+    
+    # Create the database using the function above
+    create_database(host_name, user_name, user_password, db_name)
+    
+    # Required to work with the 'finally' section
+    connection = None
+    try:
+        # Connect using credentials
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            password=user_password,
+            database=db_name
+        )
+        # If connected execute following SQL commands
+        if connection.is_connected():
+            cursor = connection.cursor()
+            # SQL commands for create the tables
+            cursor.execute("""
+                           CREATE TABLE IF NOT EXISTS CDW_SAPP_CUSTOMER (
+                               SSN VARCHAR(11) PRIMARY KEY,
+                               FIRST_NAME VARCHAR(50),
+                               MIDDLE_NAME VARCHAR(50),
+                               LAST_NAME VARCHAR(50),
+                               CREDIT_CARD_NO VARCHAR(20),
+                               FULL_STREET_ADDRESS VARCHAR(120),
+                               CUST_CITY VARCHAR(50),
+                               CUST_STATE VARCHAR(50),
+                               CUST_COUNTRY VARCHAR(50),
+                               CUST_ZIP VARCHAR(10),
+                               CUST_PHONE VARCHAR(15),
+                               CUST_EMAIL VARCHAR(100),
+                               LAST_UPDATED TIMESTAMP
+                           )
+                           """)
+            cursor.execute("""
+                           CREATE TABLE IF NOT EXISTS CDW_SAPP_BRANCH (
+                               BRANCH_CODE INT PRIMARY KEY,
+                               BRANCH_NAME VARCHAR(100),
+                               BRANCH_STREET VARCHAR(100),
+                               BRANCH_CITY VARCHAR(50),
+                               BRANCH_STATE VARCHAR(50),
+                               BRANCH_ZIP VARCHAR(10),
+                               BRANCH_PHONE VARCHAR(15),
+                               LAST_UPDATED TIMESTAMP
+                           )
+                           """)
+            cursor.execute("""
+                           CREATE TABLE IF NOT EXISTS CDW_SAPP_CREDIT_CARD (
+                               TRANSACTION_ID INT PRIMARY KEY,
+                               TIME_ID VARCHAR(8),
+                               CUST_CC_NO VARCHAR(20),
+                               CUST_SSN VARCHAR(11),
+                               BRANCH_CODE INT,
+                               TRANSACTION_TYPE VARCHAR(50),
+                               TRANSACTION_VALUE DOUBLE,
+                               FOREIGN KEY (CUST_SSN) REFERENCES CDW_SAPP_CUSTOMER(SSN) ON DELETE CASCADE,
+                               FOREIGN KEY (BRANCH_CODE) REFERENCES CDW_SAPP_BRANCH(BRANCH_CODE) ON DELETE CASCADE
+                           )
+                           """) 
+    # If error is found, print it
+    except Error as e:
+        print(f"Error: '{e}'")
+    # Close connection for good hygiene
+    finally:
+        # Check if connection is None and connection is connected
+        if connection and connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection for table creation is closed")
+
+
+# Test functions
+if __name__ == "__main__":
+    create_db_and_tables_with_keys("localhost", "root", "password", "creditcard_capstone")
